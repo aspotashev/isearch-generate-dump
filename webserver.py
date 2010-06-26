96d2539330181374bc647bff24d2cb1917ee9240
Alexander Potashev <aspotashev@gmail.com>
2010-06-26 14:48:10 +0400

    remove http server

--- webserver.py
+++ webserver.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
-#Copyright Jon Berg , turtlemeat.com
 
 import string,cgi,time
 from os import curdir, sep
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-#import pri
 
 from pology.file.catalog import Catalog
 from pology.misc.msgreport import report_msg_content
@@ -33,65 +30,8 @@ def msg_to_s(msg):
         'refentry': msg.refentry,
         })
 
-class MyHandler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        try:
-            if self.path.endswith("/cat"):
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write("<html>")
-		self.wfile.write(map(msg_to_s, cat))
-                self.wfile.write("</html>")
-            if self.path.endswith(".html"):
-                f = open(curdir + sep + self.path) #self.path has /test.html
-#note that this potentially makes every file on your computer readable by the internet
-
-                self.send_response(200)
-                self.send_header('Content-type',	'text/html')
-                self.end_headers()
-                self.wfile.write(f.read())
-                f.close()
-                return
-            if self.path.endswith(".esp"):   #our dynamic content
-                self.send_response(200)
-                self.send_header('Content-type',	'text/html')
-                self.end_headers()
-                self.wfile.write("hey, today is the" + str(time.localtime()[7]))
-                self.wfile.write(" day in the year " + str(time.localtime()[0]))
-                return
-                
-            return
-                
-        except IOError:
-            self.send_error(404,'File Not Found: %s' % self.path)
-     
-
-    def do_POST(self):
-        global rootnode
-        try:
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                query=cgi.parse_multipart(self.rfile, pdict)
-            self.send_response(301)
-            
-            self.end_headers()
-            upfilecontent = query.get('upfile')
-            print "filecontent", upfilecontent[0]
-            self.wfile.write("<HTML>POST OK.<BR><BR>");
-            self.wfile.write(upfilecontent[0]);
-            
-        except :
-            pass
-
 def main():
-    try:
-        server = HTTPServer(('localhost', 40100), MyHandler)
-        print 'started httpserver...'
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print '^C received, shutting down server'
-        server.socket.close()
+    print map(msg_to_s, cat)
 
 if __name__ == '__main__':
     main()
