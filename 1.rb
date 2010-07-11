6790cbb177f438e4f52346317ddd13e3d936bfa3
Alexander Potashev <aspotashev@gmail.com>
2010-07-11 18:06:45 +0400

    unicode (ucs-2)

--- 1.rb
+++ 1.rb
@@ -3,7 +3,8 @@
 require 'lib.rb'
 
 def dump_message_text(m)
-	m['msgid'] + m['msgstr']['*'][0]
+	s = m['msgid'] + m['msgstr']['*'][0]
+	Iconv.iconv('UCS-2', 'utf-8', s)
 end
 
 a = load_messages('/home/sasha/messages/kdebase/plasma_applet_pager.po')
