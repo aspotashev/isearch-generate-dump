306aca707241374075986561ba548a12b3cc407f
Alexander Potashev <aspotashev@gmail.com>
2010-07-13 02:34:00 +0400

    remove block

--- 1.rb
+++ 1.rb
@@ -8,7 +8,7 @@ def dump_message_text(m)
 end
 
 a = load_messages('/home/sasha/messages/kdebase/plasma_applet_pager.po')
-File.open('../dump.dat', 'w') do |f|
-	f.write a.map {|x| dump_message_text(x) }.join
-end
+
+f_dump = File.open('../dump.dat', 'w')
+f_dump.write a.map {|x| dump_message_text(x) }.join
 
