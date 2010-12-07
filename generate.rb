4aeaf2fed8d4083a54e05fcf011983919042f214
Alexander Potashev <aspotashev@gmail.com>
2010-12-08 01:30:46 +0300

    also dump messages to database

--- generate.rb
+++ generate.rb
@@ -4,6 +4,7 @@
 # http://www.gnu.org/software/gettext/manual/gettext.html#libgettextpo
 
 
+require 'active_record'
 require 'lib.rb'
 
 def dump_message_text(m)
@@ -19,11 +20,37 @@ f_mapping = File.open('../dump-map.txt', 'w')
 
 pos = 0   # number of unicode chars dumped
 
+ActiveRecord::Base.establish_connection(YAML::load(File.open('database.yml')))
+
+class CreateDb < ActiveRecord::Migration
+	def self.up
+		create_table :po_messages do |t|
+			t.string :filename
+			t.integer :index
+
+			t.string :msgid
+			t.string :msgstr
+		end
+	end
+
+	def self.down
+		drop_table :po_messages if table_exists?(:po_messages)
+	end
+end
+
+CreateDb.migrate(:down)
+CreateDb.migrate(:up)
+
+class PoMessageEntry < ActiveRecord::Base
+	set_table_name "po_messages"
+end
+
 input_files.each do |i_file|
 	puts "Parsing " + i_file
 
 	a = load_messages(i_file)
 	a.each_with_index do |x,index|
+		# Dump message to .dat file (for isearch)
 		dump_text = dump_message_text(x)
 		f_dump.write dump_text
 		f_mapping.puts "#{pos} #{i_file}:#{index}"
@@ -34,6 +61,9 @@ input_files.each do |i_file|
 			p dump_text
 			raise
 		end
+
+		# Dump message to database
+		PoMessageEntry.create(:filename => i_file, :index => index, :msgid => x['msgid'], :msgstr => x['msgstr'])
 	end
 end
 
